303f6a6ee3d71c16f7f9274dfaa62f4f194a7413
Boubaker-Soltani <boubakersol2006@gmail.com>
2026-04-21 22:39:15 +0100

    Add milk production feature to Cow class

--- Boubaker/Cow.java
+++ Boubaker/Cow.java
@@ -1,26 +1,41 @@
+
 // Class Cow (تمثيل البقرة)
 // ======================================
 import java.time.LocalDate;
 
 public class Cow extends Animal {
-
+    private double milkPerDay; // لتر حليب يوميا
     // ======================================
     // Constructor (بناء كائن البقرة)
     // ======================================
-    Cow(LocalDate birthDate, double weight) {
+
+    Cow(LocalDate birthDate, double weight, double milkPerDay) {
 
         // Call parent constructor (استدعاء الكلاس الأب Animal)
         super(birthDate, weight, "Barley, corn", "milk");
+        this.milkPerDay = milkPerDay;
     }
 
     // ======================================
-    // Override Method (إعادة تعريف الصوت)
+    // Override Method
     // ======================================
+
+    // Production الإنتاج
     @Override
-    public void makeSound() {
+    public double getProduction() {
+        if (!this.isHealthy()) {
+            System.out.println("⚠ Cow is not healthy. Treat before milking.");
+            return 0;
+        }
+        return this.milkPerDay;
+    }
+
+    // صوت البقرة
+    @Override
+    public String makeSound() {
         if (this.isHealthy())
-            System.out.println("🐄 Moo");
+            return "🐄 Moo";
         else
-            System.out.println(" Silence");
+            return " Silence";
     }
 }
